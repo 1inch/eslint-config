b6bb500c8d0df77449fe0c8583ba6dde82068d42
rharutyunyan <rado9408@gmail.com>
2026-04-28 20:23:08 +0400

    chore: ignore unused vars in catch block

--- index.mjs
+++ index.mjs
@@ -90,6 +90,7 @@ export default tseslint.config({
 
         "@typescript-eslint/no-unused-vars": ["error", {
             argsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: '^_'
         }],
 
         "unused-imports/no-unused-imports": "error",
